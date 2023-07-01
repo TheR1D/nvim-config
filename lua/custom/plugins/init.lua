1cafe14a1c9387ce45a1040ea59f0cebe57a02bf
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2023-07-01 22:35:09 +0200

    Changed default theme, cleanup unused files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,18 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+
+  -- Overwrite theme from main init.lua. Keep main init.lua in sync with main repo.
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme.
+    priority = 900, -- make sure to load this before all the other start plugins.
+    config = function()
+      require('github-theme').setup({
+        options = {
+          transparent = true,
+        }
+      })
+      vim.cmd('colorscheme github_dark')
+    end,
+  },
+
+}
